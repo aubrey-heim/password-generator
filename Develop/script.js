df03d8788717d0f887f2549ca1ce10f925face5d
aubrey-heim <aubrey.l.heim@gmail.com>
2020-09-16 22:17:35 -0700

    created character arrays and initial prompts

--- Develop/script.js
+++ Develop/script.js
@@ -11,6 +11,17 @@
 //
 // * Gather user input with prompt's and confirm's
 
+var lowerAlpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var upperAlpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+var special = ["!", "@", "#", "$", "%", "~", "&", "^", "*", "?", "<", ">"]
+
+var length = prompt("How many characters would you like your password to be?")
+var useLower = confirm('Would you like your password to contain lower-case letters? (Select "OK" for yes, or "Cancel" for no)')
+var useUpper = confirm('Would you like your password to contain upper-case letters? (Select "OK" for yes, or "Cancel" for no)')
+var useNumbers = confirm('Would you like your password to contain numbers? (Select "OK" for yes, or "Cancel" for no)')
+var useLower = confirm('Would you like your password to contain special characters? (Select "OK" for yes, or "Cancel" for no)')
+
 ///////////////////////////////////////////////////////////////////////
 // DO NOT TOUCH THIS CODE
 //
