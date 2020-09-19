ecef1681e8b4b77aa4479af2c7890caeb758b52d
aubrey-heim <aubrey.l.heim@gmail.com>
2020-09-19 14:07:22 -0700

    updating special characters

--- Develop/script.js
+++ Develop/script.js
@@ -11,10 +11,10 @@
 //
 // * Gather user input with prompt's and confirm's
 
-var lowerAlpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var upperAlpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var lowerAlpha = "abcdefghijklmnopqrstuvwxyz"
+var upperAlpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-var special = ["!", "@", "#", "$", "%", "~", "&", "^", "*", "?", "<", ">"]
+var special = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 
 function generatePassword() {
 
